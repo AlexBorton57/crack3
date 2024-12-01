ddd08547222ba6b81aa0932c1fc897d50a87bdd0
AlexBorton57 <aborton@student.sierracollege.edu>
2024-12-01 13:48:06 -0800

    crack.c

--- crack.c
+++ crack.c
@@ -14,35 +14,56 @@
 
 int main(int argc, char *argv[])
 {
+    // ensures that proper command line arguments are used
     if (argc < 3) 
     {
         printf("Usage: %s hash_file dictionary_file\n", argv[0]);
         exit(1);
     }
 
-    // TODO: Read the hashes file into an array.
-    //   Use either a 2D array or an array of arrays.
-    //   Use the loadFile function from fileutil.c
-    //   Uncomment the appropriate statement.
+    // variables
     int size;
-    //char (*hashes)[HASH_LEN] = loadFile(argv[1], &size);
-    //char **hashes = loadFile(argv[1], &size);
-    
-    // CHALLENGE1: Sort the hashes using qsort.
-    
-    // TODO
-    // Open the password file for reading.
+    int hashCount = 0;
 
-    // TODO
-    // For each password, hash it, then use the array search
-    // function from fileutil.h to find the hash.
-    // If you find it, display the password and the hash.
-    // Keep track of how many hashes were found.
-    // CHALLENGE1: Use binary search instead of linear search.
+    // 2d array containing list of hashes
+    char (*hashes)[HASH_LEN] = loadFile2D(argv[1], &size);
+    
+    // stores hash of plaintext
+    char *hash;
+    // buffer
+    char buffer[PASS_LEN];
+    // opens file containing plaintext to be hashed and compared to hash list
+    FILE *dictionary = fopen(argv[2], "r");
+    // ensures file opens properly
+    if (!dictionary)
+	{
+	    perror("Can't open file");
+	    exit(1);
+	}
 
-    // TODO
-    // When done with the file:
-    //   Close the file
-    //   Display the number of hashes found.
-    //   Free up memory.
+    // while plaintext is recieved
+    while (fgets(buffer, sizeof(buffer), dictionary) != NULL){
+        // trim newline from buffer
+        char *ref = strchr(buffer, '\n');
+        if (ref){
+            *ref = '\0';
+        }
+        // hashes plaintext
+        hash = md5(buffer, strlen(buffer));
+        // determines if hashed plaintext matches existing hash
+        char *found = stringSearch2D(hash, hashes, size);
+        if (found){
+            // print hash and plaintext, free hash, increment hashCount
+            printf("%s %s\n", hash, buffer);
+            free(hash);
+            hashCount++;
+        }
+    }
+
+    // close file and free hashes
+    fclose(dictionary);
+    free(hashes);
+    // print final matched hash count
+    printf("Hash count: %d\n", hashCount);
+    
 }
